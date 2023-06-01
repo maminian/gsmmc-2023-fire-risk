da78d81657c5f7a7fadd492aff7bef655484f7bf
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2023-06-01 13:43:20 -0700

    Create wind_prospector_vis2.py

---
+++ nrel/wind_prospector_vis2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 12:39:57 2023
+
+@author: maminian
+"""
+
+import pandas
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+# manually mined from wind-prospector 1.3G zipped folder;
+# Wind_Speed_Annual/Wind_Speed_Annual.csv
+
+# Nevermind -- this is for the gulf coastal region.
+# =============================================================================
+# df = pandas.read_csv('Wind_Speed_Annual.csv')
+# 
+# fig,ax = plt.subplots()
+# 
+# 
+# ax.scatter(
+#     df['Lon'],
+#     df['Lat'],
+#     c=df['Hour 05 Wind Speed'],
+#     vmin=0,
+#     s=1
+# )
+# 
+# fig.show()
+# =============================================================================
+
+import geopandas
+
+fig,ax = plt.subplots()
+
+# world map
+world_gdf = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+world_gdf = world_gdf[world_gdf['continent'] == 'North America']
+
+world_gdf.plot(ax=ax, facecolor=[0,0,0,0], edgecolor='k')
+
+gdf = geopandas.read_file('Western_Wind_Dataset')
+
+gdf.plot(column='CAPACITY F', ax=ax, s=1)
+
+fig.show()
