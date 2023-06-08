dc61da023c252071b2fba418793b1735b89cabea
Claire Chang <claires.chang@gmail.com>
2023-06-08 16:28:00 -0400

    Add wind data to the df using interpolation

--- kmeans/clean_data.py
+++ kmeans/clean_data.py
@@ -2,8 +2,11 @@ import numpy as np
 import urllib.request
 import json
 import pickle
+import geopandas
+import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
 
-savefile = "data.pickle"  # CHANGE to save data to a different file
+savefile = "co_data.pickle"  # CHANGE to save data to a different file
 base_url = "http://data.rcc-acis.org/"
 
 #######################################
@@ -58,18 +61,43 @@ params = {
 temp_elev_data = GridData(params, server="GridData")
 
 precip = np.array(precip_data['smry'][0])
+shapex,shapey = precip.shape
 elev = np.array(temp_elev_data['meta']['elev'])
 lat, lon = np.array(temp_elev_data['meta']['lat']), np.array(temp_elev_data['meta']['lon'])
 temp = np.array(temp_elev_data['smry'][0])
-
 # Note: the lat and lon coordinates line up, so we only need to extract them from one of the jsons.
-all_data = np.reshape(np.dstack((lat,lon,elev,precip,temp)),(99*171,5))
+all_data = np.reshape(np.dstack((lat,lon,elev,precip,temp)),(shapex*shapey,5))
 
+# process wind and other data into pandas df
+gdf = geopandas.read_file('Western_Wind_Dataset')
+gdf["lon"] = gdf['geometry'].x
+gdf["lat"] = gdf['geometry'].y
+gdf = pd.DataFrame(gdf.drop(columns=['geometry']))
+df = pd.DataFrame(all_data,columns=["lat","lon","elev","precip","temp"])
+
+# interpolate wind data
+ll = np.dstack((gdf[['lat']],gdf[['lon']]))[:,0]
+nbrs = KNeighborsRegressor(n_neighbors=5).fit(ll,gdf[['CAPACITY F']])
+ll2 = np.dstack((df[['lat']],df[['lon']]))[:,0]
+df["cf"] = nbrs.predict(ll2)
+cf = np.array(df["cf"]).reshape((shapex,shapey))
+
+# normalize entries for precip, elev, temp for kmeans
+precip_n = precip / np.amax(precip)
+elev_n = elev / np.amax(elev)
+temp_n = temp / np.amax(temp)
+cf_n = cf / np.amax(cf)
+all_data = np.reshape(np.dstack((lat,lon,elev_n,precip_n,temp_n,cf_n)),(shapex*shapey,6))
+
+# save all data as dictionary
 d = {"precip":precip,
     "temp":temp,
     "elev":elev,
     "lat":lat,
     "lon":lon,
-    "all_data":all_data}
+    "cf":cf,
+    "all_data":all_data
+    }
+
 with open(savefile, 'wb') as handle:
     pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
