f32bcf6e3eadbabaa17724cd9b9120ce245d0067
Claire Chang <claires.chang@gmail.com>
2023-06-09 15:26:19 -0400

    Aggregate data for multiple states (fix hacky version)

--- kmeans/clean_data.py
+++ kmeans/clean_data.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsRegressor
 
 base_url = "http://data.rcc-acis.org/"
+wind_data_dir = "data/Western_Wind_Dataset"
 
 ############################# Web Scraping ##########################################
 
@@ -27,6 +28,27 @@ def make_request(url,params) :
 
 def GridData(params, server="GridData"):
     return make_request(base_url+server, params)
+
+############################## Process Wind Data #########################################
+
+def scrape_wind_data(non_wind_data):
+    # read in wind data
+    gdf = geopandas.read_file(wind_data_dir)
+    gdf["lon"] = gdf['geometry'].x
+    gdf["lat"] = gdf['geometry'].y
+    gdf = pd.DataFrame(gdf.drop(columns=['geometry']))
+    df = pd.DataFrame(non_wind_data,columns=["lat","lon","elev","precip","temp"])
+
+    # drop NaN so that the k-means algorithm can process the df
+    df = df.dropna()
+
+    # interpolate wind data
+    ll = np.dstack((gdf[['lat']],gdf[['lon']]))[:,0]
+    nbrs = KNeighborsRegressor(n_neighbors=5).fit(ll,gdf[['CAPACITY F']])
+    ll2 = np.dstack((df[['lat']],df[['lon']]))[:,0]
+    df["cf"] = nbrs.predict(ll2)
+
+    return df
 
 ############################## Scrape Precipitation, Temperature, Elevation Data #########################################
 
@@ -56,42 +78,34 @@ def scrape_data(state,sdate="20200101",edate="20201231"):
     temp_elev_data = GridData(params, server="GridData")
 
     # stack the precipitation, elevation, and temperature data with lat/lon
-    precip = np.array(precip_data['smry'][0])
-    shapex,shapey = precip.shape
-    elev = np.array(temp_elev_data['meta']['elev'])
-    lat, lon = np.array(temp_elev_data['meta']['lat']), np.array(temp_elev_data['meta']['lon'])
-    temp = np.array(temp_elev_data['smry'][0])
+    precip = np.array(precip_data['smry'][0]).flatten()
+    elev = np.array(temp_elev_data['meta']['elev']).flatten()
+    lat, lon = np.array(temp_elev_data['meta']['lat']).flatten(), np.array(temp_elev_data['meta']['lon']).flatten()
+    temp = np.array(temp_elev_data['smry'][0]).flatten()
+
+    # clean the precipitation, elevation, temp data since some numbers aren't valid
+    precip = precip.astype(float)
+    precip[precip < 0] = np.nan
+    elev = elev.astype(float)
+    elev[elev < 0] = np.nan
+    temp = temp.astype(float)
+    temp[temp < 150] = np.nan
+
     # Note: the lat and lon coordinates line up, so we only need to extract them from one of the jsons.
-    all_data = np.reshape(np.dstack((lat,lon,elev,precip,temp)),(shapex*shapey,5))
-
-############################## Process Wind Data #########################################
-
-    # read in wind data
-    gdf = geopandas.read_file('data/Western_Wind_Dataset')
-    gdf["lon"] = gdf['geometry'].x
-    gdf["lat"] = gdf['geometry'].y
-    gdf = pd.DataFrame(gdf.drop(columns=['geometry']))
-    df = pd.DataFrame(all_data,columns=["lat","lon","elev","precip","temp"])
-
-    # interpolate wind data
-    ll = np.dstack((gdf[['lat']],gdf[['lon']]))[:,0]
-    nbrs = KNeighborsRegressor(n_neighbors=5).fit(ll,gdf[['CAPACITY F']])
-    ll2 = np.dstack((df[['lat']],df[['lon']]))[:,0]
-    df["cf"] = nbrs.predict(ll2)
-    cf = np.array(df["cf"]).reshape((shapex,shapey))
-
-    return shapex,shapey,precip,temp,elev,lat,lon,cf,all_data
+    non_wind_data = np.dstack((lat,lon,elev,precip,temp))[0]
+    df = scrape_wind_data(non_wind_data)
+    return df
 
 ############################## Organize and Save All Data into Pickle File #########################################
 
-def pickle_dump(data,savefile):
-    shapex,shapey,precip,temp,elev,lat,lon,cf,all_data = data
-    # normalize entries for precip, elev, temp for kmeans
+def pickle_dump(df,savefile):
+    precip,temp,elev,lat,lon,cf = np.array(df["precip"]),np.array(df["temp"]),np.array(df["elev"]),np.array(df["lat"]),np.array(df["lon"]),np.array(df["cf"])
+    # normalize entries for precip, elev, temp, cap factor for kmeans
     precip_n = precip / np.amax(precip)
     elev_n = elev / np.amax(elev)
     temp_n = temp / np.amax(temp)
     cf_n = cf / np.amax(cf)
-    all_data = np.reshape(np.dstack((lat,lon,elev_n,precip_n,temp_n,cf_n)),(shapex*shapey,6))
+    all_data = np.reshape(np.dstack((lat,lon,elev_n,precip_n,temp_n,cf_n)),(-1,6))
 
     # save all data as dictionary in a pickle file
     d = {"precip":precip,
@@ -106,13 +120,15 @@ def pickle_dump(data,savefile):
         pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
     return
 
-def save_data(state):
+def save_data(states):
     '''
     Main wrapper function to save to pickle file
     Input: state - all caps 2 letter abbreviation (ex. CO)
     Output: None - saves a pickle file (ex. data/co_data.pickle)
     '''
-    savefile = "data/" + state.lower() + ".pickle"
-    data = scrape_data(state)  # default sdate, edate: the entire year of 2020
-    pickle_dump(data,savefile)
+    savefile = "data/" + "_".join(map(lambda x: x.lower(), states)) + ".pickle"
+    all_dfs = []
+    for state in states:
+        all_dfs.append(scrape_data(state))  # default sdate, edate: the entire year of 2020
+    pickle_dump(pd.concat(all_dfs),savefile)
     return
