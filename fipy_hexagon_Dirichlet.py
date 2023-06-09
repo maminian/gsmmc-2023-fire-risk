d8f4dd2555a8dc52ac2b525df214b66db0095c93
arn97 <arnabroy@udel.edu>
2023-06-09 17:18:53 -0400

    Create fipy_hexagon_Dirichlet.py

---
+++ fipy_hexagon_Dirichlet.py
@@ -0,0 +1,103 @@
+from fipy import Variable, FaceVariable, CellVariable, Grid1D, Grid2D, ExplicitDiffusionTerm, TransientTerm, DiffusionTerm, Viewer
+from fipy.tools import numerix
+import numpy as np
+import math,mpmath
+from matplotlib import pyplot as plt
+
+#A function to define the polygonal mesh
+def gen_mesh(pts,cellSize):
+
+    import fipy
+    
+    gmsh_arg = 'cellSize = %.8f; \n '%cellSize    # %.8f could cause truncation issues in edge cases
+    idx = 0
+    for p in pts:
+        idx += 1
+        gmsh_arg += 'Point(%i) = {%.8f,%.8f,0,%.8f}; \n '%(idx, p[0], p[1], cellSize)
+    #
+
+    line_ptrs = [str(jj+1+idx) for jj in range(len(pts))]
+    for j in range(len(pts)):
+        idx += 1
+        gmsh_arg += 'Line(%i) = {%i,%i};\n'%(idx, 1+(j%len(pts)), 1+((j+1)%len(pts)))
+        
+#        line_ptrs += str(idx)
+    #
+
+    idx += 1
+    gmsh_arg += 'Line Polygon(%i) = '%(idx,)
+    gmsh_arg += '{' + ','.join(line_ptrs) + '}; \n'
+
+    idx += 1
+    gmsh_arg += 'Plane Surface(%i) = {%i};\n'%(idx, idx-1)
+
+    # Call fipy to construct the mesh with the given 
+    # input parameters (a plaintext file we constructed procedurally)
+    mesh = fipy.Gmsh2D(gmsh_arg)
+
+    return mesh
+
+#Creating the an hexagonal mesh with a downward slope at the bottom
+x_max=2
+x_min=-0.5*x_max
+x_length=x_max-x_min
+y_min=0
+theta = np.pi/6
+y_max=2
+h=-x_min
+slope_base=h*mpmath.cot(theta)
+left_length=(x_length-slope_base)/2
+right_length=left_length
+pts = [(x_min,h),(left_length+x_min, h), (x_max-right_length, y_min), (x_max,y_min), (x_max,y_max), (x_min, y_max)]
+
+mesh = gen_mesh(pts, 0.01)
+#Defining the potential variable
+phi = CellVariable(name="Wind speed",
+                   mesh=mesh,
+                   value=0.)
+
+#Defining the equation
+D=1
+eqX = DiffusionTerm(coeff=D)==-2
+
+#Setting up boundary conditions. This is the simple case of Dirichlet case.
+#The assumption is The wind is coming into and getting out of our system
+#at a constant speed, the motion of wind will not be effected much by the
+#shape of the surface and the wind speed will be zero at the surface.
+
+valueLeft=2
+valueRight=2
+valueTop=2
+valueBottomRight=0
+valueBottomLeft=0
+valueBottomMiddle=0
+
+X, Y=mesh.faceCenters
+
+facesbottomright=(mesh.exteriorFaces & (X>=x_max-right_length) & (Y==y_min)) 
+facesbottomleft=(mesh.exteriorFaces & (X<left_length+x_min) & (Y==h)) 
+facesbottommiddle=(mesh.exteriorFaces & (X>=left_length+x_min) & (X<x_max-right_length) & (Y<=h+0.1))
+faceslefttop=(mesh.exteriorFaces & (X==x_min))
+facesright=(mesh.exteriorFaces & (X==x_max))
+facestop=(mesh.exteriorFaces & (Y==y_max))
+phi.constrain(valueTop,facestop)
+phi.constrain(valueBottomRight,facesbottomright)
+phi.constrain(valueBottomMiddle, facesbottommiddle)  
+phi.constrain(valueBottomLeft, facesbottomleft)
+phi.constrain(valueRight, facesright) 
+phi.constrain(valueLeft, faceslefttop)  
+
+#Plotting the graph
+viewer = Viewer(vars=phi)
+eqX.solve(var=phi)
+viewer.plot()
+
+fig=plt.gcf()
+ax=fig.gca()
+ax.tricontour(mesh.cellCenters[0],mesh.cellCenters[1],phi.value,40)
+plt.xlabel("Ground position")
+plt.ylabel("Height")
+
+
+
+
