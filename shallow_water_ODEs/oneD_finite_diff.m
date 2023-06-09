51475056984f6da74bd72b564e969927562ffcd1
op554 <op554@nyu.edu>
2023-06-09 09:41:24 -0400

    Added latitude and longitude to elevation profile

--- shallow_water_ODEs/oneD_finite_diff.m
+++ shallow_water_ODEs/oneD_finite_diff.m
@@ -2,30 +2,49 @@ clear;
 clc;
 close all
 
-elev_data = load('test.mat');
-data = cell2mat(struct2cell(elev_data));
+elev_data = load('elev.mat');
+lat_data = load('lat.mat');
+lon_data = load('lon.mat');
+elev = cell2mat(struct2cell(elev_data));
+lat = cell2mat(struct2cell(lat_data));
+lon = cell2mat(struct2cell(lon_data));
 
 figure;
-surf(data);
+surf(lat,lon,elev);
 colorbar
 hold on
+xlabel('Latitude');
+ylabel('Longitude');
+zlabel('Elevation');
 
 
+% plot slice as plane
+desired_lat = 40.2;
+[~,ind] = min( abs( lat(:,1)-desired_lat ) );
 
 
-slice = data(30,:);
-new_h = smoothdata(slice);
+slice = elev(ind,:);
+new_h = smoothdata(slice,'gaussian');
 
 
 figure;
 hold on
-plot(slice);
-plot(new_h);
+plot(lon(1,:),slice,'LineWidth',2);
+plot(lon(1,:),new_h,'LineWidth',2);
+title(['Slice at latitude = ', num2str(lat(ind,1))]);
+xlabel('Longitude');
+ylabel('Elevation');
+set(gca,'TickLength',[0.02, 0.05]);
+set(gca,'LineWidth',1);
+box on
+
+ax = gca;
+ax.FontSize = 15;
 
 
 N = size(new_h,2);
 
-xspan = linspace(-1,2,N);
+xspan = lon(1,:);
 %h = exp(-((xspan-1)/.1).^2)/2;
 %h = -exp(-((xspan-1/2)/.1).^2)/2 + exp(-(xspan/.1).^2)/2;
 h = new_h;
@@ -36,16 +55,21 @@ g = 9.8;
 %% ode45 (Runge-Kutta)
 
 
-u1 = 180; % only JUST big enough to break!!
-Dvals = 10000; % subcritical
+%u1 = 270;
+D1 = 10000; % subcritical
 %uvals = 12;
+
+uvals = [240 270 300];
+Fr_init = uvals.^2/(g*D1);
+
+% plot Froude number on separate vertical axis
 
 figure;
 hold on
 %plot(xspan,h,'LineWidth',2);
 
-for j=1:size(Dvals,2)
-    D1 = Dvals(j);
+for j=1:size(uvals,2)
+    u1 = uvals(j);
 
 
     %[xspan,y] = ode45(@(x,y) fun(x,y,xspan,dh,g), xspan, [u1; D1]);
@@ -54,17 +78,18 @@ for j=1:size(Dvals,2)
 %     u = y(:,1);
 %     D = y(:,2);
 
-u = y;
-D = u1*D1./y;
-    
+    u = y;
+    D = u1*D1./y;
     Fr = u.^2./(g*D);
-        hold on
+
+
+    hold on
     
     %subplot(3,2,j);
     %plot(xspan,D+h','LineWidth',2);
     plot(xspan,u,'LineWidth',2);
     %plot(xspan, u.*D);
-    plot(xspan,Fr,'LineWidth',2);
+    %plot(xspan,Fr,'LineWidth',2);
 
     
 %     str = ['Fr = ', num2str(Fr)];
@@ -76,9 +101,11 @@ D = u1*D1./y;
 
     %title('D=1, u=12, Fr=15');
     
-    xlabel('x');
+    xlabel('Longitude');
+    ylabel('Wind speed');
 
-    legend('flow speed','Froude number');
+    legend(num2str(Fr_init'));
+    title(['Slice at latitude = ', num2str(lat(ind,1))]);
 
     set(gca,'TickLength',[0.02, 0.05]);
     set(gca,'LineWidth',1);
@@ -90,78 +117,15 @@ end
 
 
 
-
-
-% %% forward Euler -- DO NOT USE
-% u = zeros(N,1);
-% D = zeros(N,1);
-% ds = (xspan(end) - xspan(1)) / N;
-% % u(1) = 1;
-% % D(1) = 1;
-% 
-% u(1) = 6;
-% Dvals = [1/2, 1, 3, 6, 8];
-% 
-% figure;
-% 
-% for j=1:size(Dvals,2)
-%     D(1) = Dvals(j);
-% 
-%     Fr = u(1)^2/(g*D(1)^2);
-% 
-%     for i=1:N-1
-%         D(i+1) = D(i) + ds * (1 - g*D(i)/u(i)^2)^(-1) * g * D(i)/u(i)^2 * dh(i);
-%     %     if D(i+1) < h(i+1) % make sure the fluid height doesn't drop below the mountain
-%     %         D(i+1) = h(i+1);
-%     %     end
-%         u(i+1) = u(i) * (1 - (D(i+1)-D(i))/D(i));
-%     end
-%     
-%     
-%     subplot(3,2,j);
-%     plot(xspan,D+h');
-%     hold on
-%     plot(xspan,h);
-%     
-%     str = ['Fr = ', num2str(Fr)];
-%     title(str);
-%     
-%     legend('D','h');
-%     
-%     xlabel('x');
-%     ylabel('y');
-% 
-%     set(gca,'TickLength',[0.02, 0.05]);
-%     set(gca,'LineWidth',1);
-% 
-%     ax = gca;
-%     ax.FontSize = 15;
-% end
-
-
 %% functions
 
-function dydx = fun(x,y,xspan,dh,g)
-dh_an = interp1(xspan,dh,x);
-dydx = [-g*dh_an ./ (y(1)-y(2)./y(1)); -g*dh_an ./ (-y(1).^2./y(2)+g)];
-end
+% handles the shock worse
+% function dydx = fun(x,y,xspan,dh,g)
+% dh_an = interp1(xspan,dh,x);
+% dydx = [-g*dh_an ./ (y(1)-y(2)./y(1)); -g*dh_an ./ (-y(1).^2./y(2)+g)];
+% end
 
 function dydx_1D = fun2(x,y,xspan,dh,g,u1,D1)
 dh_an = interp1(xspan,dh,x);
 dydx_1D = -g*dh_an ./ (y-(g*u1*D1)./y.^2);
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
